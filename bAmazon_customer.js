9f7773db4807aed114e1786a94a5ab7c348009ad
Sean Baklini <sbaklini@unm.edu>
2020-04-24 08:40:51 -0600

    Database configuring properly

--- bAmazon_customer.js
+++ bAmazon_customer.js
@@ -1,21 +1,23 @@
 let mysql = require('mysql');
 let inquirer = require('inquirer');
 require("dotenv").config();
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.bamazonPassword,
+    database: "bamazon"
+})
 class Bamazon {
     //creating connection to database
     constructor() {
-        this.connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: process.env.bamazonPassword,
-            database: "bamazon"
-        })
+
     }
 
 
-    start = function () {
-        this.connection.query('SELECT * FROM products', function (err, res) { //selecting all products from the products table
+
+    start =  ()=> {
+        connection.query('SELECT * FROM products', function (err, res) { //selecting all products from the products table
             if (err) throw err;
             console.table(res);
             inquirer.prompt([{
@@ -30,6 +32,25 @@ class Bamazon {
             }
             ]).then(function (answers) {
                 console.log(answers);
+              connection.query('SELECT * FROM ?? WHERE ID = ?', ["products", answers.id], function (err, res2) {
+                   // [{id:3,quanitty:10}]
+                    console.table(res2);
+
+                    console.log(answers.quantity, res2[0].QUANTITY);
+                    if(res2[0].QUANTITY < parseInt(answers.QUANTITY)) {
+                        console.log("Insufficient quantity.")
+                    }
+                    else {
+                        let subtraction = res2[0].QUANTITY - answers.quantity;
+                       let update = connection.query('UPDATE ?? SET ?? = ? WHERE ?? = ?',["products", "QUANTITY", subtraction, "id", answers.id], function(err, res3){
+                            this.start();
+                        })
+
+                        console.log(update.sql);
+                    }
+                })
+
+                //console.log(statement.sql);
             })
 
         }
