22f3dbff4116be0175131725050f5ce5c8b4c2e8
Sean Baklini <sbaklini@unm.edu>
2020-04-23 20:25:41 -0600

    Added classes

--- bAmazon_customer.js
+++ bAmazon_customer.js
@@ -1,38 +1,44 @@
-let mysql = require('my sql');
+let mysql = require('mysql');
 let inquirer = require('inquirer');
+require("dotenv").config();
+class Bamazon {
+    //creating connection to database
+    constructor() {
+        this.connection = mysql.createConnection({
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: process.env.bamazonPassword,
+            database: "bamazon"
+        })
+    }
 
-//creating connection to database
-let connection = mysql.createConnection({
-    host: "local host",
-    port: 3306,
-    user: "root",
-    password: " ",
-    database: "Bamazon"
-})
 
-let start = function () {
-    connection.query('SELECT * FROM PRODUCTS', function (err, res) { //selecting all products from the products table
-        if (err) throw err;
-        for (let i = 0; i < res.length; i++) {
-            console.log(res[i].itemID + res[i].productName + res[i].prices);
+    start = function () {
+        this.connection.query('SELECT * FROM products', function (err, res) { //selecting all products from the products table
+            if (err) throw err;
+            console.table(res);
+            inquirer.prompt([{
+                type: "number",
+                name: "id",
+                message: "What is the ID of the product?",
+            },
+            {
+                type: "number",
+                name: "quantity",
+                message: "How much of the product would you like to buy?",
+            }
+            ]).then(function (answers) {
+                console.log(answers);
+            })
+
         }
-        inquirer.prompt([{
-            type: "number",
-            name: "id",
-            message: "What is the ID of the product?",
-
-
-        },
-        {
-            type: "number",
-            name: "quantity",
-            message: "How much of the product would you like to buy?",
-        }
-        ])
+        )
 
     }
-    ).then(function () {
 
 
-    })
 }
+
+let bamazon = new Bamazon();
+bamazon.start();
