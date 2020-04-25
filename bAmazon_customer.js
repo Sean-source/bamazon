1c72f6ee4286e3ab790635c4c12c214790b584b1
Sean Baklini <sbaklini@unm.edu>
2020-04-25 07:35:30 -0600

    Working customer file

--- bAmazon_customer.js
+++ bAmazon_customer.js
@@ -1,23 +1,29 @@
 let mysql = require('mysql');
 let inquirer = require('inquirer');
 require("dotenv").config();
-let connection = mysql.createConnection({
+let connect = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: process.env.bamazonPassword,
     database: "bamazon"
 })
+let total = 0;
 class Bamazon {
-    //creating connection to database
-    constructor() {
 
+
+    //creating connection to database
+    constructor(connect, total) {
+        this.connection = connect;
+        this.total = total; 
     }
 
 
 
-    start =  ()=> {
-        connection.query('SELECT * FROM products', function (err, res) { //selecting all products from the products table
+    start = function () {
+
+      //  console.log(this.name)
+        this.connection.query('SELECT * FROM products', (err, res)=> { //selecting all products from the products table
             if (err) throw err;
             console.table(res);
             inquirer.prompt([{
@@ -30,23 +36,24 @@ class Bamazon {
                 name: "quantity",
                 message: "How much of the product would you like to buy?",
             }
-            ]).then(function (answers) {
-                console.log(answers);
-              connection.query('SELECT * FROM ?? WHERE ID = ?', ["products", answers.id], function (err, res2) {
+            ]).then((answers) =>{
+               // console.log(answers);
+              this.connection.query('SELECT * FROM ?? WHERE ID = ?', ["products", answers.id], (err, res2) =>{
                    // [{id:3,quanitty:10}]
                     console.table(res2);
 
-                    console.log(answers.quantity, res2[0].QUANTITY);
-                    if(res2[0].QUANTITY < parseInt(answers.QUANTITY)) {
+                    // console.log(answers.quantity, res2[0].QUANTITY);
+                    if(res2[0].QUANTITY < answers.quantity) {
                         console.log("Insufficient quantity.")
+                        this.start();
                     }
                     else {
+                        this.total += answers.quantity * res2[0].PRICE;
+                        console.log("This is your total cost: $" + this.total);
                         let subtraction = res2[0].QUANTITY - answers.quantity;
-                       let update = connection.query('UPDATE ?? SET ?? = ? WHERE ?? = ?',["products", "QUANTITY", subtraction, "id", answers.id], function(err, res3){
+                       let update = this.connection.query('UPDATE ?? SET ?? = ? WHERE ?? = ?',["products", "QUANTITY", subtraction, "id", answers.id], (err, res3)=>{
                             this.start();
                         })
-
-                        console.log(update.sql);
                     }
                 })
 
@@ -61,5 +68,5 @@ class Bamazon {
 
 }
 
-let bamazon = new Bamazon();
+let bamazon = new Bamazon(connect, total);
 bamazon.start();
